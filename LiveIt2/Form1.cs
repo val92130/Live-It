9d910f1399c8cc220a4699f1907d67c52022e32f
val92130 <chatelain@intechinfo.fr>
2014-11-17 11:30:05 +0100

    map fixes

---
+++ LiveIt2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LiveIt2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics g;
+        Bitmap btm;
+        Rectangle r;
+        Pen p;
+        public void DrawMethod(Brush b)
+        {
+            
+            
+            p = new Pen( b);
+            r = new Rectangle( 0, 0, 100, 100 );
+            btm = new Bitmap( r.Width + 1, r.Height + 1 );
+            g = Graphics.FromImage(btm);
+            g.DrawRectangle( p, r );
+
+        }
+
+        private void button1_Click( object sender, EventArgs e )
+        {
+            DrawMethod( Brushes.Black );
+            pictureBox1.Image = btm; 
+          
+        }
+
+        private void pictureBox1_MouseLeave( object sender, EventArgs e )
+        {
+            DrawMethod( Brushes.Red );
+            pictureBox1.Image = btm; 
+        }
+
+        private void pictureBox1_MouseEnter( object sender, EventArgs e )
+        {
+            DrawMethod( Brushes.Blue );
+            pictureBox1.Image = btm; 
+        }
+    }
+}
