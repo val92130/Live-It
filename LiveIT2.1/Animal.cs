d504b28e26ed62d84f956f595a28de8e2e7ebdd8
Van Palamar <rami.morri@gmail.com>
2014-11-18 12:51:22 +0100

    class Animal

---
+++ LiveIT2.1/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveIT2._1
+{
+    public class Animal
+    {
+        Size _size;
+        Point _position;
+        Map _map;
+
+        public Animal( Map map )
+        {
+            _map = map;
+            _size = new Size( 50, 50 );
+            Random r = new Random();
+            _position = new Point( r.Next( 0, _map.MapSize ), r.Next( 0, _map.MapSize ) );
+        }
+
+
+        public Point Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+
+        public Size Size
+        {
+            get { return _size; }
+            set { _size = value; }
+        }
+
+
+    }
+}
